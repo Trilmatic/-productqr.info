3ec140961c4fbd0ede524b63ec84e3390cda5ddf
p.suchanek <P.Suchanek@nonstop-recruitment.com>
2022-12-28 20:00:56 +0100

    remove gtin

--- database/migrations/2022_12_25_134806_create_products_table.php
+++ database/migrations/2022_12_25_134806_create_products_table.php
@@ -18,7 +18,6 @@ return new class extends Migration
             $table->string('hash')->nullable()->default(null);
             $table->string('name');
             $table->string('identification_code')->nullable()->default(null);
-            $table->integer('gtin')->nullable()->default(null);
             $table->foreignId('user_id')->references('id')->on('users');
             $table->timestamps();
             $table->softDeletes();
